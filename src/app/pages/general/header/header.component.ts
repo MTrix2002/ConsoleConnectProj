d7416a5f4cd481f2b1bb25cda890b4e6745de2c7
Ossie MOUANGASSA <MinuMouangassa@Outlook.com>
2024-04-18 12:02:08 +0200

    added Logo

--- src/app/pages/general/header/header.component.ts
+++ src/app/pages/general/header/header.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

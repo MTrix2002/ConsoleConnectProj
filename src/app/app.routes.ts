4dd23f1dbb761b85707e28f0c8c65f0cf27b097b
Ossie MOUANGASSA <MinuMouangassa@Outlook.com>
2024-04-22 15:00:04 +0200

    started settings need to finish tonight

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { FriendsComponent } from './pages/general/friends/friends.component';
 import { HomePageComponent } from './pages/general/home-page/home-page.component';
 import { NotFoundComponent } from './pages/general/not-found/not-found.component';
 import { ProfileComponent } from './pages/general/profile/profile.component';
+import { SettingsComponent } from './pages/general/settings/settings.component';
 
 export const routes: Routes = [
 
   {path: '', component: HomePageComponent},
   {path: 'MyProfile',component: ProfileComponent},
   {path: 'About',component: AboutComponent},
-  {path: 'Friends',component: FriendsComponent},
+  {path: 'Subscriptions',component: FriendsComponent},
+  {path: 'Settings',component: SettingsComponent},
 
   {path: '**', component: NotFoundComponent},
 ];
